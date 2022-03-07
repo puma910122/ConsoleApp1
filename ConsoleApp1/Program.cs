a9999e2c2f6f7084925ea1545255efed99d42bc4
user <user@DESKTOP-6JBEF5K>
2022-03-07 12:04:23 +0800

    2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,14 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            double meal_cost = 100;
+            int tip_parcent = 15;
+            int tax_parcent = 8;
+            int meal_costint = (int)meal_cost;
+            int sum = meal_costint + tip_parcent + tax_parcent;
+            Console.WriteLine("totalcost:"+sum);
+
         }
     }
 }
